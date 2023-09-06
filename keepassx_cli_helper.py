40b5256243ad554a6a797f240e453e80dae494be
Dick Marinus <dick@mrns.nl>
2023-09-06 21:10:21 +0200

    extract

--- keepassx_cli_helper.py
+++ keepassx_cli_helper.py
@@ -4,3 +4,9 @@ def show(secret_tool_attribute, secret_tool_value, keepassx_cli_database, keepas
     keypass = subprocess.check_output(['secret-tool', 'lookup', secret_tool_attribute, secret_tool_value])
     p = subprocess.Popen(['keepassxc-cli', 'show', keepassx_cli_database, keepassx_cli_entry, '-a', 'Password'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     return p.communicate(keypass)[0].split(b'\n')[1]
+
+def extract(secret_tool_attribute, secret_tool_value, keepassx_cli_database):
+    keypass = subprocess.check_output(['secret-tool', 'lookup', secret_tool_attribute, secret_tool_value])
+    p = subprocess.Popen(['keepassxc-cli', 'extract', keepassx_cli_database], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    output = p.communicate(keypass)[0]
+    return output[output.find(b"<?xml"):]
